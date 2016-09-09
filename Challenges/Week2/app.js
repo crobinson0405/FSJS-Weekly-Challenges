d4394b0ac847706b91003721970638c0f0e27242
Christopher Robinson <lget6@hotmail.com>
2016-09-08 20:00:19 -0400

    WEEK 2 JS CHALLENGE

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -2,7 +2,22 @@
   //control and return statements
 
 //Prompt user for input and store variable
+function processInput(message)
+{
+if(message == "Hello")
+{
+  return("Hello world!");
+}
+  else {
+    return("You did not say Hello!");
+  }
+}
 
+var userInput;
+
+userInput = prompt();
+
+processInput(userInput);
 //Call function
 
 //Alert user results
